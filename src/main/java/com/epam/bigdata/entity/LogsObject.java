79887739c9885750ce929b011e88c9eb815c94f7
Ilya_Starushchanka <ilya10194@mail.ru>
2016-10-04 12:21:40 +0300

    read dataset

---
+++ src/main/java/com/epam/bigdata/entity/LogsObject.java
@@ -0,0 +1,39 @@
+package com.epam.bigdata.entity;
+
+import java.util.Date;
+
+/**
+ * Created by Ilya_Starushchanka on 10/4/2016.
+ */
+public class LogsObject {
+
+    private Date date;
+    private int userTagsId;
+    private long cityId;
+
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date time) {
+        this.date = time;
+    }
+
+    public int getUserTagsId() {
+        return userTagsId;
+    }
+
+    public void setUserTagsId(int userTagsId) {
+        this.userTagsId = userTagsId;
+    }
+
+    public long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(long cityId) {
+        this.cityId = cityId;
+    }
+
+}
--- src/main/java/com/epam/bigdata/spark/SparkApp.java
+++ src/main/java/com/epam/bigdata/spark/SparkApp.java
@@ -1,5 +1,6 @@
 package com.epam.bigdata.spark;
 
+import com.epam.bigdata.entity.LogsObject;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
@@ -7,8 +8,17 @@ import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -29,7 +39,25 @@ public class SparkApp {
                 .builder()
                 .appName("JavaWordCount")
                 .getOrCreate();
-        JavaRDD<String> lines = spark.read().text(args[0]).javaRDD();
+
+        JavaRDD<LogsObject> logsRDD = spark.read().text(args[0]).javaRDD().map(new Function<String, LogsObject>() {
+            @Override
+            public LogsObject call(String line) throws Exception {
+                String[] parts = line.split("\\s+");
+
+                LogsObject logsObject = new LogsObject();
+                logsObject.setUserTagsId(Integer.parseInt(parts[parts.length-2]));
+                logsObject.setCityId(Integer.parseInt(parts[parts.length-15]));
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+                String dateInString = parts[1].substring(0,8);
+                Date date = formatter.parse(dateInString);
+                logsObject.setDate(date);
+                return logsObject;
+            }
+        });
+
+        /*JavaRDD<String> lines = spark.read().text(args[0]).javaRDD();
 
         JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
             @Override
@@ -57,7 +85,9 @@ public class SparkApp {
         List<Tuple2<String, Integer>> output = counts.collect();
         for (Tuple2<?,?> tuple : output) {
             System.out.println(tuple._1() + ": " + tuple._2());
-        }
+        }*/
         spark.stop();
     }
+
+
 }
